cd670624d57ab6f0480262192daff8ae0629e3f4
SonOfTenny <mark.tennyson1@gmail.com>
2015-11-25 20:38:39 +0000

    updated

--- phpselect.php
+++ phpselect.php
@@ -65,5 +65,70 @@
 	</div>
 </div>
 
+<!-- editModal -->
+<div id="editModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Edit Record</h4>
+      </div>
+      <div class="modal-body">
+        <form role="form">
+        	<div class="form-group">
+        		<label for="username">Username:</label>
+        		<input type="text" name="username" class="form-control" id="username" value="<?php echo $username; ?>">
+        	</div>
+        	<div class="form-group">
+        		<label for="fname">First Name:</label>
+        		<input type="text" name="firstName" class="form-control" id="fname" value="<?php echo $firstName; ?>">
+        	</div>
+        	<div class="form-group">
+        		<label for="lname">Last Name:</label>
+        		<input type="text" name="lastName" class="form-control" id="lname" value="<?php echo $lastName; ?>">
+        	</div>
+        	<div class="form-group">
+        		<label for="password">Password:</label>
+        		<input type="password" name="password" class="form-control" id="password" value="<?php echo $password; ?>">
+        	</div>
+        	<!-- Submit button for editing-->
+        	<input type="submit" onclick="return confirm('Are you sure you want to save?')"name="submit" value="Submit">
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
 </body>
 </html>
+
+<!-- PHP Magic down here -->
+<?php
+// connect to Shift_Reports
+include('connectdb.php');
+// Check to see if form was submitted, start to process the
+// form and save it to the database
+if (isset($_POST['submit'])) {
+	// get form data, making sure it is valid
+	$firstName = htmlspecialchars($_POST['firstName']);
+	$lastName = htmlspecialchars($_POST['lastName']);
+	$username = htmlspecialchars($_POST['username']);
+	$password = htmlspecialchars($_POST['password']);
+
+	// check to make sure fields are complete
+	//if ($firstName == '' || $lastName == '') {
+	//	$error = 'Error: Please fill in all required fields';
+	//}
+	// save to the database
+	sqlsrv_query("INSERT INTO dbo.Tuser(username,password,firstName,LastName)
+					VALUES($username,$password,$firstName,$lastName)");
+	or die(print_r(sqlsrv_errors(),true));
+
+}
+
+?>

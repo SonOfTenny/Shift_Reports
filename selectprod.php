0cace94bef0524c32edadfb855932a41545ade25
SonOfTenny <mark.tennyson1@gmail.com>
2015-11-27 21:46:40 +0000

    updates

--- selectprod.php
+++ selectprod.php
@@ -36,16 +36,39 @@ if ($stmt === false) {
 				</tr>	
 			</thead>
 			<tbody>
-				<?php while ($row = sqlsrv_fetch_array($stmt,SQLSRV_FETCH_ASSOC)) {
-					echo '<tr><td>'.$row['id'].'</td><td>'.$row['userID'].'</td><td>'.$row['plantID'].'</td><td>'.
-					$row['shiftID'].'</td><td>'.$row['prod_hours'].'</td><td>'.$row['actual_mix'].'</td><td>'.
-					$row['crumb_waste'].'</td><td>'.$row['cmp_waste'].'</td><td>'.$row['manning'].'</td><td>'.
-					$row['date'].'</td><td></tr>';
-				}?>
+				<?php 
+				// Connect to Shift_Reports
+				$connInfo = array('UID' =>'mark','PWD'=>'Password1',
+				'Database'=>'Shift_Reports' );
+				$serverName = '(local)';
+				$conn = sqlsrv_connect($serverName,$connInfo);
+				if($conn===false){
+					die(print_r(sqlsrv_errors(),true));
+				}
+				// setup and execute the query
+				$sql = "SELECT * FROM dbo.Tuser";
+				$stmt = sqlsrv_query($conn,$sql);
+				// retrieve each row as an associative array
+				// and display the results
+				while ($row = sqlsrv_fetch_array($stmt,SQLSRV_FETCH_ASSOC)) {
+					echo '<tr><td>'.$row['ID'].'</td>
+								<td>'.$row['userID'].'</td>
+								<td>'.$row['plantID'].'</td><td>'
+									.$row['shiftID'].'</td>
+								<td>'.$row['prod_hours'].'</td>
+								<td>'.$row['actual_mix'].'</td>
+								<td>'.$row['crumb_waste'].'</td>
+								<td>'.$row['cmp_waste'].'</td>
+								<td>'.$row['manning'].'</td>
+								<td>'.$row['date'].'</td></tr>';
+
+				// free statement and connection resources
+				sqlsrv_free_stmt($stmt);
+				sqlsrv_close($conn);
+				?>
 
 			</tbody>
 		</table>
-		sqlsrv_free_stmt($stmt);
-		sqlsrv_close($conn);
+
 </body>
 </html>

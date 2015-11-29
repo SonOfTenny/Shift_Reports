5567e39b148bf78b4cc66f04a358e2c60da522b3
SonOfTenny <mark.tennyson1@gmail.com>
2015-11-29 13:06:09 +0000

    all files updated

--- selectdown.php
+++ selectdown.php
@@ -1,20 +1,18 @@
 <html>
-<head></head>
 <body>
-<table>
-<thead>
+<table >
+			<thead>
 				<tr>
 					<td>ID</td>
 					<td>User ID</td>
 					<td>Plant ID</td>
-					<td>Shift Type</td>
-					<td>Production Hours</td>
-					<td>Actual Mix</td>
-					<td>Crumb Waste</td>
-					<td>Compactor Waste</td>
-					<td>Manning</td>
+					<td>Dtype ID</td>
+					<td>Shift ID</td>
+					<td>Down Hours</td>
+					<td>Reason</td>
+					<td>Action</td>
 					<td>Date</td>
-				</tr>	
+				</tr>
 			</thead>
 			<tbody>
 				<?php 
@@ -27,29 +25,24 @@
 					die(print_r(sqlsrv_errors(),true));
 				}
 				// setup and execute the query
-				$sql = "SELECT * FROM dbo.Tproddata";
+				$sql = "SELECT * FROM dbo.Tdowntdata";
 				$stmt = sqlsrv_query($conn,$sql);
 				// retrieve each row as an associative array
 				// and display the results
 				while ($row = sqlsrv_fetch_array($stmt,SQLSRV_FETCH_ASSOC)) {
 					echo '<tr><td>'.$row['ID'].'</td>
 								<td>'.$row['userID'].'</td>
-								<td>'.$row['plantID'].'</td><td>'
-									.$row['shiftID'].'</td>
-								<td>'.$row['prod_hours'].'</td>
-								<td>'.$row['actual_mix'].'</td>
-								<td>'.$row['crumb_waste'].'</td>
-								<td>'.$row['cmp_waste'].'</td>
-								<td>'.$row['manning'].'</td>
-								<td>'.$row['date'].'</td></tr>';
+								<td>'.$row['plantID'].
+								'</td><td>'.$row['dtypeID'].'</td>
+										<td>'.$row['shiftID'].'</td>
+											<td>'.$row['down_hours'].'</td>
+												<td>'.$row['reason'].'</td>
+													<td>'.$row['action'].'</td>
+														<td>'.$row['date'].'</td></tr>';
 				}
 				// free statement and connection resources
 				sqlsrv_free_stmt($stmt);
 				sqlsrv_close($conn);
 				?>
-
-			</tbody>
-		</table>
-
 </body>
 </html>

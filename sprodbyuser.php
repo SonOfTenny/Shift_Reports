aef796af9ac7f176199f20250cc1159b672364c9
SonOfTenny <mark.tennyson1@gmail.com>
2015-11-29 14:58:15 +0000

    upd

---
+++ sprodbyuser.php
@@ -0,0 +1,50 @@
+<html>
+<body>
+<table >
+			<thead>
+				<tr>
+					<td>ID</td>
+					<td>First Name</td>
+					<td>Last Name</td>
+					<td>Plant</td>
+				</tr>
+			</thead>
+			<tbody>
+				<?php 
+				// Connect to Shift_Reports
+				$connInfo = array('UID' =>'mark','PWD'=>'Password1',
+				'Database'=>'Shift_Reports' );
+				$serverName = '(local)';
+				$conn = sqlsrv_connect($serverName,$connInfo);
+				if($conn===false){
+					die(print_r(sqlsrv_errors(),true));
+				}
+				// setup and execute the query
+				$sql = "select Tuser.firstName as 'firstName', Tuser.lastName as 'lastName', Tplant.name as 'plant', 
+						Tplant.mrph as 'mrph', Tshift.type as 'sType', Tproddata.prod_hours as 'prod_hours', 
+						Tproddata.actual_mix as 'actual',
+						Tproddata.crumb_waste as 'crumb', Tproddata.cmp_waste as 'cmp', 
+						Tproddata.manning as 'manning' FROM Tuser
+						INNER JOIN Tproddata
+						ON Tproddata.userID = Tuser.id
+						inner join Tplant
+						on Tproddata.plantID = Tplant.id
+						inner join Tshift
+						on Tproddata.shiftID = Tshift.id";
+				$stmt = sqlsrv_query($conn,$sql);
+				// retrieve each row as an associative array
+				// and display the results
+				while ($row = sqlsrv_fetch_array($stmt,SQLSRV_FETCH_ASSOC)) {
+					echo '<tr><td>'.$row['firstName'].'</td>
+								<td>'.$row['lastName'].
+								'</td><td>'.$row['plant'].'</td></tr>';
+				}
+				// free statement and connection resources
+				sqlsrv_free_stmt($stmt);
+				sqlsrv_close($conn);
+
+				?>
+			</tbody>
+		</table>
+
+</body>
